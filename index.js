9cf0fcfff3e3cdd402e3042bc3b4fb4d63735875
Kejvin Pashaj <kevin.952011@gmail.com>
2023-02-25 13:25:48 +0000

    inquirer prompts structure

--- index.js
+++ index.js
@@ -12,11 +12,103 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+console.log("Please build your team");
 
 inquirer
     .prompt([
-
+        // add manager information for starter
+        {
+            type: "input",
+            name: "name",
+            message: "What is the team manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the team manager's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the team manager's email?"
+        },
+        {
+            type: "input",
+            name: "officeNum",
+            message: "What is the team manager's office number?"
+        }
     ]).then(response => {
-        
+
     })
 
+const promptChoice = () => {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "id",
+                message: "Which type of team member would you like to add?",
+                choices: ["Engineer",
+                    "Intern",
+                    "Don' want to add any more team members"]
+            }
+        ]).then(response => {
+
+        })
+}
+
+const promptEngineer = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is your engineer's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your engineer's id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your engineer's email?"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is your engineer's GitHub username?"
+            },
+        ]).then(response => {
+
+        })
+}
+
+const promptIntern = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is your intern's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your intern's id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your intern's email?"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is your intern's School?"
+            },
+        ]).then(response => {
+
+        })
+}
